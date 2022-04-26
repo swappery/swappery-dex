bb5f5f567332560d478e48847056f65dae65e873
rainbowemoji <devzone210@gmail.com>
2022-04-26 13:07:51 +0900

    context built

---
+++ tests/src/swappery_router_test.rs
@@ -0,0 +1,8 @@
+#[derive(Copy, Clone)]
+struct TestContext {
+    erc20_token0: ContractHash,
+    erc20_token1: ContractHash,
+    wcspr_token: ContractHash,
+    swappery_pair_0_1: ContractPackageHash,
+    swappery_router: ContractPackageHash,
+};
